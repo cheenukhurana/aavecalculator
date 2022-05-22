f16b104cc9148e87827ffc75071cd74fecc9c45f
Cheenu Khurana <cheenu.ck@gmail.com>
2022-05-22 13:48:52 +0530

    remove layout css and add tailwind

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,22 @@
 import Head from 'next/head'
 import styles from './layout.module.css'
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>Aave Calculator</title>
-                <link rel="icon" href="/favicon.ico" />
-                <meta name="viewport" content="initial-scale=1, width=device-width" />
-            </Head>
-            <header className={styles.header}>
-                <h1>Aave Calculator</h1>
-            </header>
-            <main>
-                {children}
-            </main>
+        <div className='bg-[#5f3473] h-screen'>
+            <div className='max-w-2xl mx-auto py-4'>
+                <Head>
+                    <title>Aave Calculator</title>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta name="viewport" content="initial-scale=1, width=device-width" />
+                </Head>
+                <header className="text-center">
+                    <h1>Aave Calculator</h1>
+                </header>
+                <main>
+                    {children}
+                </main>
+            </div>
         </div>
     )
 }

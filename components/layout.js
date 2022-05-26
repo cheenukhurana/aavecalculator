4633c77d635e8b1e81c95b18a04ee6b680ddf86b
Cheenu Khurana <cheenu.ck@gmail.com>
2022-05-26 15:39:37 +0530

    google analytics

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,29 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function Layout({ children }) {
+
+    const injectGA = () => {
+        if (typeof window == 'undefined') {
+          return;
+        }
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          window.dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+      
+        gtag('config', 'G-F39B5S38ST');
+      };
+
     return (
         <div className='bg-gradient-to-br from-[#5f3473] to-[#71c1d6] h-screen'>
             <div className='max-w-xl mx-auto py-4'>
                 <Head>
+                    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-F39B5S38ST"></Script>
+                    <Script>
+                        {injectGA()}
+                    </Script>
                     <title>Aave Calculator</title>
                     <link rel="icon" href="/favicon.ico" />
                     <meta name="viewport" content="initial-scale=1, width=device-width" />
